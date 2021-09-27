89bb741481e871f94524afaefdfb2e80dc9b423e
CrawlTheDev <crawlthedev@gmail.com>
2021-09-27 08:28:02 -0700

    add recommended extensions

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,16 +1,21 @@
 module.exports = {
-    extends: ["stylelint-config-recommended"],
-    rules: {
-        "at-rule-no-unknown": [true, {
-            ignoreAtRules: [
-                "tailwind",
-                "apply",
-                "variants",
-                "responsive",
-                "screen",
-            ],
-        }],
-        "declaration-block-trailing-semicolon": null,
-        "no-descending-specificity": null,
-    },
+  extends: ["stylelint-config-recommended"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen",
+          "layer",
+        ],
+      },
+    ],
+    "declaration-block-trailing-semicolon": null,
+    "no-descending-specificity": null,
+    "no-invalid-position-at-import-rule": null,
+  },
 };
